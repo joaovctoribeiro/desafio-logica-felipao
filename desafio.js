ad77adb5e161cabfa169b27abfaf2ba50243936f
João Ribeiro <joaovctoribeiro@gmail.com>
2023-10-28 20:05:34 -0300

    Desafio de Lógica de Programação.;

---
+++ desafio.js
@@ -0,0 +1,44 @@
+let nomeHeroi = "RibeiroHeroi";
+let xpHeroi = 8079;
+
+  if(xpHeroi <= 1000) {
+
+    let nivel = "Ferro";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+  } else if(xpHeroi <= 2000 || xpHeroi == 1001) {
+
+    let nivel = "Bronze";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+  } else if(xpHeroi <= 5000 || xpHeroi == 2001) {
+
+    let nivel = "Prata";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+  } else if(xpHeroi <= 7000 || xpHeroi == 6001) {
+
+    let nivel = "Ouro";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+ } else if(xpHeroi <= 8000 || xpHeroi == 7001) {
+    
+    let nivel = "Platina";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+ } else if(xpHeroi <= 9000 || xpHeroi == 8001) {
+    
+    let nivel = "Ascendente";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+ } else if(xpHeroi <= 10000 || xpHeroi == 9001) {
+    
+    let nivel = "Imortal";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+ } else if(xpHeroi >= 10001) {
+
+    let nivel = "Radiante";
+    console.log("O Herói "+nomeHeroi+" está no Nível "+nivel+"! Ótimo progresso.");
+
+ }
